eb3acb3100a73a2445e0e935ae79c5a4caa7e093
amit42 <44144209+amit42@users.noreply.github.com>
2019-05-25 09:50:45 +0530

    replaced vector with array

--- count_inversion.cpp
+++ count_inversion.cpp
@@ -2,83 +2,85 @@
 #include <fstream>
 #include<vector>
 using namespace std;
-int inv_count(vector<int> &vect,int l,int r);
-int merge_count(vector<int> &vect,int l,int m,int r);
+long long int merge(int arr[], int l, int m, int r);
+long long int mergeSort(int arr[], int l, int r);
 vector<int> readfile(char[50]);
 int main()
 {
-    vector<int> v {1,3,2,4,5,6};
-    v = readfile("IntegerArray.txt");
-    cout<<inv_count(v,0,v.size()-1)<<" ";
-	return 0;
-}
-vector<int> readfile(char str[50])
-{
     fstream myfile;
-	vector<int> v;
-	myfile.open(str);
+	int arr[100000];
+	myfile.open("IntegerArray.txt");
+	long long int result;
+	int i=0;
 	int a;
 	while(myfile >> a)
 	{
-		v.push_back(a);
+		arr[i] = a;
+		++i;
 	}
-	return v;
+    result = mergeSort(arr,0,100000-1);
+    cout<<result;
+	return 0;
 }
-int inv_count(vector<int> &vect,int l,int r)
+
+long long int merge(int arr[], int l, int m, int r)
 {
-    if (l<r)
-    {
-        int m = l + (r-l)/2 ;
-        long long int x = inv_count(vect,l,m);
-        long long int y = inv_count(vect,m+1,r);
-        long long int z = merge_count(vect,l,m,r);
-        return x+y+z;
-    }
-    else
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	int L[n1], R[n2];
+
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1+ j];
+	i = 0;
+	j = 0;
+	k = l;
+	long long int counter = 0;
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+			counter += n1-i;
+		}
+		k++;
+	}
+	return counter;
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+long long int mergeSort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		int m = l+(r-l)/2;
+		long long int x = mergeSort(arr, l, m);
+		long long int y = mergeSort(arr, m+1, r);
+
+		long long int z = merge(arr, l, m, r);
+
+        return x+y+z ;
+	}
+	else
         return 0;
 }
-int merge_count(vector<int> &vect,int l,int m,int r)
-{
-	vector<int> vl;
-	vector<int> vr;
-    for(int i=l;i<=r;i++)
-    {
-    	if(i<m)
-    		vl.push_back(vect[i]);
-    	else
-    		vr.push_back(vect[i]);
-    }
-    int i = 0;
-    int j = 0;
-    int k =l;
-    long long int counter = 0;
-    while(i < vl.size() && j < vr.size())
-    {
-    	if (vl[i]<=vr[j])
-    	{
-    		vect[k]= vl[i] ;
-    		++i;
-    	}
-    	else
-    	{
-    		vect[k] = vr[j] ;
-    		++j;
-    		counter += (vl.size()-i);
-    	}
-    	++k;
-    }
-    while(i < vl.size())
-    {
-    	vect[k]=vl[i];
-    	++i;
-    	++k;
-    }
-    while(j < vr.size())
-    {
-    	vect[k]=vr[j];
-    	++j;
-    	++k;
-    }
-    return counter;
 
-}
+

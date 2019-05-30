23fa0c0bfd3f1d7a3a90319f6e73f93701e8809f
amit42 <44144209+amit42@users.noreply.github.com>
2019-05-30 16:47:19 +0530

    collatz

--- classic/collatz.cpp
+++ classic/collatz.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+using namespace std;
+int collatz(int x);
+int main()
+{
+	int n;
+	cout<<"enter the number";
+	cin >>n;
+	if(n<=1)
+		cout<<"invalid input";
+	else
+		cout<<"number of steps to reach 1 :"<<collatz(n);
+	return 0;
+}
+int collatz(int x)
+{
+	int steps = 0;
+	while(x!=1)
+	{
+		if(x%2==0)
+			x/=2;
+		else
+			x = 3*x+1;
+		steps++;
+	}
+	return steps;
+}
